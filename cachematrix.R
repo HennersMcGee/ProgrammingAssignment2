0bf1b16f91c20cef30ecfabe2417a14dc424dacb
HennersMcGee <henry_letton@hotmail.com>
2019-05-30 17:47:50 +0100

    Applied own changes to forked project. Should be ready to submit.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2
+## Caching the Inverse of a Matrix
+# This program consists of two function.
+# The first function creates a special "matrix" object that can cache it's inverse
+# The second function computes the inverse of the matrix or retrieves an already calculated inverse
 
-## Write a short comment describing this function
+## The first function is called "makeCacheMatrix". Input of matrix x.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The second functon is called "cacheSolve". Input of matrix x.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        #If it retrieves already calculated data, it's prints this message.
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    message("no cached data")
+    m
 }
+
+
+
+#Test below, commented for submission
+#y <- cbind(c(1,5,3,2),c(4,5,7,20),c(7,8,8,8),c(1,2,3,4))
+#y2 <- makeCacheMatrix(y)
+#y3 <- cacheSolve(y2)
+#y3
